fd0421fa1e443f35a3768fe7f830f90010909f56
janvaljean <kurtuldu@gmx.de>
2023-04-05 06:40:53 +0200

    t lists created and to Tasklist comp as props send

--- form-context-server-project/src/App.jsx
+++ form-context-server-project/src/App.jsx
@@ -1,21 +1,28 @@
-
-import './App.css'
-import TaskCreate from './components/TaskCreate'
-import TaskList from './components/TaskList'
+import { useState } from "react";
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
 
 function App() {
-    const createTask = (header, task) => {
-     console.log(header,task)
-     
-    };
-
-
+  const [tasks, setTasks] = useState([]);
+  const createTask = (header, task) => {
+    const createdTasks = [
+      ...tasks,
+      {
+        id: new Date().getMilliseconds(),
+        header,
+        task,
+      },
+    ];
+    setTasks(createdTasks);
+  };
+  console.log(tasks);
   return (
     <div className="App">
-      <TaskCreate onCreate={createTask}/>
-      <TaskList/>
+      <TaskCreate onCreate={createTask} />
+      <TaskList tasks={tasks} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
